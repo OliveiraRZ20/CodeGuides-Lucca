a083cf2a0c33a6d78b7787b841835e794760d77c
OliveiraRZ20 <lucca0505@gmail.com>
2024-02-21 20:46:02 -0300

    Create 7 CodeGUIDE Matplotlib.py

---
+++ 7 CodeGUIDE Matplotlib.py
@@ -0,0 +1,22 @@
+# instruções de como se utilizar a biblioteca Matplotlib, sendo mais utilizada na criação
+# de gráficos para analise de dados e visualização de dataframes
+
+# o primeiro de tudo é importar a biblioteca para o seu arquivo
+# P.S = normalmente utilizamos o nome mpl por que é mais facil de chamar quando for executar comandos
+
+import matplotlib as mpl
+
+# a segunda coisa é adicionar outra biblioteca que personaliza os gráficos gerados pelo mpl, ela se chama
+# pyplot, então é so importar ela junto, lembrando que ela faz parte do mpl, então fica asssim:
+
+import matplotlib.pyplot as plt
+
+# Se estiver usando um JUPYTER NOTEBOOK:
+# Uma opção interessante é fazer o mpl gerar os graficos na mesma pagina e não ficar abrindo outras
+# janelas com o seguinte comando:
+
+'''
+%matplotlib inline
+'''
+
+
